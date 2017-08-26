70afd252540066b3c24b435ceb14cd70cc7f32e5
musha <mushagh@gmail.com>
2017-08-26 23:09:13 +0800

    finish login page

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <!--<link rel="stylesheet" href="css/style.css">-->
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/font-awesome.min.css">
     <link rel="icon" href="img/fw1.png">
     <title>GreatWall | Firewall</title>
--- index.php
+++ index.php
@@ -8,39 +8,37 @@
 
     if($username == "admin" && $password == "240be518fabd2724ddb6f04eeb1da5967448d7e831c08c8fa822809f74c720a9"){
       $_SESSION['login_user'] = $username;
-      header("location: main.php");
+      header("location: admin-main.php");
     } else {
       header("location: index.php");
     }
   }
 ?>
-<body>
-  <section class="section1">
-    <div class="container">
-      <div class="row">
-        <div style="margin-top:30px;" class="col-md-4 col-md-offset-4">
-          <img class="img-responsive" src="img/fw1.png">
-          <h3 align="center">GreatWall</h3>
-          <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-            <div class="form-group">
-              <div class="input-group">
-                <span class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i></span>
-                <input type="text" class="form-control" placeholder="Username" name="username">
-              </div>
+<section class="section1">
+  <div class="container">
+    <div class="row">
+      <div style="margin-top:30px;" class="col-md-4 col-md-offset-4">
+        <img class="img-responsive" src="img/fw1.png">
+        <h3 align="center">GreatWall</h3>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+          <div class="form-group">
+            <div class="input-group">
+              <span class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i></span>
+              <input type="text" class="form-control" placeholder="Username" name="username">
             </div>
-            <div class="form-group">
-              <div class="input-group">
-                <span class="input-group-addon"><i class="fa fa-lock" aria-hidden="true"></i></span>
-                <input type="password" class="form-control" placeholder="Password" name="password">
-              </div>
+          </div>
+          <div class="form-group">
+            <div class="input-group">
+              <span class="input-group-addon"><i class="fa fa-lock" aria-hidden="true"></i></span>
+              <input type="password" class="form-control" placeholder="Password" name="password">
             </div>
-            <div class="form-group">
-              <button type="submit" name="login" class="btn btn-success btn-block">Login</button>
-            </div>
-          </form>
-        </div>
+          </div>
+          <div class="form-group">
+            <button type="submit" name="login" class="btn btn-success btn-block">Login</button>
+          </div>
+        </form>
       </div>
     </div>
-  </section>
-</body>
+  </div>
+</section>
 <?php require 'footer.php'; ?>
